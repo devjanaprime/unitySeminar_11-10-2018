e008702498602bab888408b7203757aa3b37405c
Dev Jana <dev@primeacademy.io>
2018-11-10 15:22:19 -0600

    end of seminar

--- Assets/scripts/physicsObject0.cs
+++ Assets/scripts/physicsObject0.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class physicsObject0 : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+    private string lastCollidedWith = "";
+    private int juggle = 0;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -18,22 +19,33 @@ public class physicsObject0 : MonoBehaviour {
             Camera.main.SendMessage("spawnPhysicsObject");
             Destroy(gameObject);
         } // fell too far
-        print(Random.Range(0, 100));
     }
 
     private void OnCollisionEnter(Collision collision)
-    {  
+    {
         // Goal?
-        if( collision.collider.tag == "Goal"){
+        if ( collision.collider.tag == "Goal"){
             // increment score
             print("Goal");
             // delete this object
+            Camera.main.SendMessage("spawnPhysicsObject");
+            Camera.main.SendMessage("spawnPhysicsObject");
             Camera.main.SendMessage("spawnPhysicsObject");
             Camera.main.SendMessage("goal");
             Destroy(gameObject);
         } // end Goal
         if( collision.collider.tag == "Player" ){
-            GetComponent<Rigidbody>().AddForce(0, 50, 0);
+            GetComponent<Rigidbody>().AddForce(0, collision.collider.transform.localScale.x * 50, 0);
+            if(lastCollidedWith == "Player"){
+                juggle++;
+                if( juggle >1){
+                    collision.collider.transform.localScale = new Vector3(collision.collider.transform.localScale.x * 1.1f, collision.collider.transform.localScale.y * 1.1f, collision.collider.transform.localScale.z * 1.1f);
+                } //end scale up for juggle
+            } // end player juggle
+            else{
+                juggle = 0;
+            } // end juggle
         } // end Player
+        lastCollidedWith = collision.collider.tag;
     } // end OnCollisionEnter
 } // end class
---
+++ Assets/scripts/triggerHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("entered trigger: " + other.tag);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print("exited trigger: " + other.tag);
+    }
+}
